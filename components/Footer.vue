a58351a410e124fbe668201271873c1c9515f146
Iban Moreno Martin <iban.moreno.martin@gmail.com>
2020-06-14 12:53:50 +0200

    copyleft on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,26 @@
 <template>
 
-    <footer class="bg-teal-400 h-18 py-8">
+    <footer class="bg-teal-400 py-8 flex justify-center items-center">
 
-        <p class="text-white text-center">Anno Domini 2020</p>
+        <Copyleft />
+
+        <p class="text-white text-center"><strong class="font-bold pl-4">Amparo</strong> | Anno Domini 2020</p>
 
     </footer>
 
 </template>
 
 <script>
+
+    import Copyleft from '~/components/Copyleft'
+
     export default {
+
+        components: {
+
+            Copyleft
+
+        }
         
     }
 </script>
