e6e0cb609d257b09398a86b44e04108a4b08bd7d
Iban Moreno <iban.moreno.martin@gmail.com>
2021-05-24 04:52:19 +0200

    cambio de tesxto en buscador

--- components/Search.vue
+++ components/Search.vue
@@ -2,13 +2,13 @@
 
     <div class="py-8">
 
-        <p class="font-bold text-sm mb-1">Busca nombres de producto, hay 200, ej.: Producto 123 (hay algunos productos que tienen "styx river" en el nombre) prueba a buscar esos</p>
+        <p class="font-bold text-sm mb-1">Buscador de productos, hay 200, ej.: Producto 123 (hay algunos productos que tienen "styx river" en el nombre) prueba a buscar esos</p>
 
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Busca en 200 productos" v-model="q">
 
         <div v-if="results.length" class="grid grid-cols-2 md-grid-cols-3 lg-grid-cols-6 gap-4">
 
-            <SearchResult v-for="(product,index) in results" 
+            <SearchResult v-for="(product,index) in results"
                 :key="index"
                 :product="product"
             />
@@ -58,12 +58,12 @@
                 }
 
                 this.searching = true
-                
+
                 this.results = await this.$content('products')
                     .search(q)
                     .sortBy('id')
                     .fetch({ deep: true })
-                
+
                 this.searching = false
 
             }
