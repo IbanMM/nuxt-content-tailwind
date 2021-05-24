0b46993078b467d99a5263e40a7009bd1f75c7f9
Iban Moreno <iban.moreno.martin@gmail.com>
2021-05-24 05:44:21 +0200

    cambio texto buscador continuous deploys

--- components/Search.vue
+++ components/Search.vue
@@ -2,7 +2,7 @@
 
     <div class="py-8">
 
-        <p class="font-bold text-sm mb-1">Buscador de productos, hay 200, ej.: Producto 123 (hay algunos productos que tienen "styx river" en el nombre) prueba a buscar esos</p>
+        <p class="font-bold text-sm mb-1">Buscador de productos subida con continuous deploys, hay 200, ej.: Producto 123 (hay algunos productos que tienen "styx river" en el nombre) prueba a buscar esos</p>
 
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Busca en 200 productos" v-model="q">
 
